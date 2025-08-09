ce74b78c47237af7431bc9b9b4ccdb4a85ebce17
jie <jk3308@126.com>
2025-08-09 15:13:47 +0800

    feat: 头像上传接口

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,10 +8,13 @@ import { CustomExpectionFilter } from './common/custom-expection.filter';
 const configService = new ConfigService();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExpectionFilter());
+  app.useStaticAssets('uploads', {
+    prefix: '/uploads',
+  });
   await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,26 @@
-import { Body, Controller, Get, ParseBoolPipe, ParseIntPipe, Post, Query, SetMetadata } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  ParseBoolPipe,
+  ParseFilePipe,
+  ParseIntPipe,
+  Post,
+  Query,
+  SetMetadata,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { UpdateUserInfoDto } from './dto/update-info.dto';
 import { AssignRoleDto } from './dto/assign-role.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path';
+import { storage } from 'src/common/upload.storage';
 
 @Controller('user')
 export class UserController {
@@ -68,5 +84,31 @@ export class UserController {
   @Get('froze')
   frozeUser(@Query('id', ParseIntPipe) id: number, @Query('status', ParseBoolPipe) status: boolean) {
     return this.userService.frozeUser(id, status ? 1 : 0);
+  }
+
+  @SetMetadata('requireLogin', true)
+  @Post('upload/avatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads/avatar',
+      limits: {
+        fileSize: 1024 * 1024 * 2,
+      },
+      storage: storage('uploads/avatar'),
+      fileFilter: (req, file, cb) => {
+        if (['.png', '.jpg', '.jpeg'].includes(path.extname(file.originalname).toLowerCase())) {
+          cb(null, true);
+        } else {
+          cb(new BadRequestException('只能上传png、jpg、jpeg图片'), false);
+        }
+      },
+    }),
+  )
+  uploadAvatar(@UploadedFile('file', ParseFilePipe) file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('上传失败');
+    }
+
+    return `uploads/avatar/${file.filename}`;
   }
 }
